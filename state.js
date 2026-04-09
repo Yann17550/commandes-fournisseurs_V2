21d25b9c871786749fcae1b3fc7c6a20b8515d5c
Yann17550 <millebulle@gmail.com>
2026-04-09 11:49:00 +0200

    Update state.js

--- state.js
+++ state.js
@@ -2,9 +2,6 @@
 //  STATE GLOBAL + UTILITAIRES GÉNÉRAUX
 // ============================================================
 
-window.__FILE_VERSIONS__ = window.__FILE_VERSIONS__ || {};
-window.__FILE_VERSIONS__["state.js"] = "2026-04-06T18:31:00";
-
 // ---- State --------------------------------------------------
 let state = {
   etab: null,
