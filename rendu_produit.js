24bf2e25e41e4b7bf917f6b6efe32263322bf55c
Yann17550 <millebulle@gmail.com>
2026-04-24 12:54:58 +0200

    Update rendu_produit.js

--- rendu_produit.js
+++ rendu_produit.js
@@ -23,13 +23,15 @@ function renderProduitAB(p, isVariant, state) {
       </div>
 
       <div class="line2_ab">
-        <span class="reference_ab product-ref${hasOverride ? ' ref-override' : ''}">${escHtml(d.reference)}</span>
+        <div class="ref-edit_ab">
+          <span class="reference_ab product-ref${hasOverride ? ' ref-override' : ''}">${escHtml(d.reference)}</span>
+          <button class="edit-btn edit-btn-ab" data-key="${escHtml(key)}" type="button" title="Modifier l'article">✏️</button>
+        </div>
 
         <div class="stepper_ab">
           <button class="qty-btn" data-key="${escHtml(key)}" data-delta="-1">−</button>
           <input class="qty-input" type="number" min="0" step="1" value="${qtyColis}" data-key="${escHtml(key)}" inputmode="numeric">
           <button class="qty-btn" data-key="${escHtml(key)}" data-delta="1">+</button>
-          <button class="edit-btn" data-key="${escHtml(key)}" type="button" title="Modifier l'article">✏️</button>
           <span class="total_ab">${qtyColis > 0 ? fmtPriceNoEuro(totalLigne) : ''}</span>
         </div>
       </div>
